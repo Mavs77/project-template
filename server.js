078aa8afa86f2e5d39f6d2094bf9e973bc6ebde1
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-09-20 21:01:55 -0500

    connected to DB

--- server.js
+++ server.js
@@ -1,78 +1,40 @@
 //Ensure this is at the top. ENsures that environment variables are loaded and available for use throughout your application. This ensures that all parts of your application have access to the necessary configuration values from the .env file.
 
-require('dotenv').config({path: './config/.env'}); 
+require('dotenv').config();  
 
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
-// const passport = require('passport')
-// const session = require('express-session') //manage user sessions in an express.js application 
-// const MongoStore = require('connect-mongo') //store session data in a mongoDB database
-const methodOverride = require('method-override')
-// const flash = require('express-flash')
-// const logger = require('morgan')
-const connectDB = require('./config/database')
-// const mainRoutes = require('./routes/main')
-// const recipeRoutes = require('./routes/recipe')
 
-//ignore
-// const commentRoutes = require('./routes/comments')
-
-// const DB_STRING = process.env.DB_STRING
 
 
 // connectDB()
 
 mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.nb0y2.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`)
+.then(() => console.log('MongoDB Connected'))
+.catch(err => console.log(err))
 
-require('./config/passport')(passport)
+// require('./config/passport')(passport)
 
+
+//Middleware 
+
+//enable CORS
+app.use(cors())
 
 // use EJS for views 
 app.set('view engine', 'ejs')
 
-//static folder 
+//static folder. Telling the server where our static files will be stored. 
 app.use(express.static('public'))
 
-//body parsing 
+//body parsing. Used to handle incoming requests and parse their data
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json()) 
 
-//logging 
-app.use(logger('dev'))
-
-//use forms for put / delete 
-app.use(methodOverride("_method"));  
-
-// // Setup sessions - stored in MongoDB
-// app.use(
-//     session({
-//         secret: 'keyboard cat', 
-//         resave: false, 
-//         saveUninitialized: false, 
-//         store: MongoStore.create({ 
-//             mongoUrl: DB_STRING
-//         })
-//     })
-// )
-
-
-// // Passport middleware 
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// //Use flash messages for errors, info, etc...
-// app.use(flash())
-
-// //setup routes for which the server is listening 
-// app.use('/', mainRoutes)
-// app.use('/recipe', recipeRoutes)
-
-// //ignore
-// app.use('/comment', commentRoutes)
  
 app.listen(process.env.PORT || 2900, ()=>{
-    console.log('Server is running, game on!')
+    console.log(`Server is running on: http://localhost:${process.env.PORT}!`)
 })
 
