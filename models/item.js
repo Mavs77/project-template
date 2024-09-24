3537fc1d352a525994452539590620586d98091c
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-09-23 22:06:25 -0500

    adding comments

--- models/item.js
+++ models/item.js
@@ -1,3 +1,5 @@
+//The code below is setting up a structure for storing items with a name and description in your database
+
 const mongoose = require("mongoose");
 
 const ItemSchema = new mongoose.Schema({
